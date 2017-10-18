558fe6ba5a6201693c6a266a139c81a45eda64f8
E.Panteleymonchuk <e.panteleymonchuk@nullgr.com>
2017-10-18 23:39:09 +0300

    Done adding elements

--- src/App.js
+++ src/App.js
@@ -3,19 +3,75 @@ import './App.css';
 
 class App extends Component {
 
-  static stat
+  constructor() {
+    super();
+
+    this.state = {
+      inputValue: '',
+      todoList: []
+    };
+  }
+
+  onInputChange = (e) => {
+    this.setState({
+      inputValue: e.target.value
+    });
+  };
+
+  handleAddToList = () => {
+
+    const todoItem = {
+
+    };
+
+    this.setState({
+      todoList: [...this.state.todoList, this.state.inputValue]
+    });
+
+    this.clearInput();
+  };
+
+  clearInput = () => {
+    this.setState({
+      inputValue: ''
+    });
+  };
 
   render() {
+
+    const { inputValue, todoList } = this.state;
+
+    console.log(todoList);
+
     return (
       <div className="App">
         <div className="todo">
           <div className="todo-input">
-
+            <input type="text" value={inputValue} onChange={this.onInputChange}/>
+            <button onClick={this.handleAddToList}>Add</button>
           </div>
           <div className="todo-list">
             <ul>
-              <li></li>
+              {
+                !!todoList.length ? (
+                    <ul>
+                      {
+                        todoList.map((item, i) => (
+                          <li htmlFor={`todo-number-${i}`} key={`todo-number-${i}`}>
+                            <input type="checkbox" id={`todo-number-${i}`} onClick={() => alert(1)}/>
+                            {item}
+                          </li>
+                        ))
+                      }
+                    </ul>
+                  ) : (
+                    <div>
+                      empty
+                    </div>
+                  )
+              }
             </ul>
+
           </div>
         </div>
       </div>
