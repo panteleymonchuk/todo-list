772e57c6f839963fdee7ac5bfb8728d9767f25da
E.Panteleymonchuk <e.panteleymonchuk@nullgr.com>
2017-10-18 22:50:36 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  static stat
+
+  render() {
+    return (
+      <div className="App">
+        <div className="todo">
+          <div className="todo-input">
+
+          </div>
+          <div className="todo-list">
+            <ul>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
